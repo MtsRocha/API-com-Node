f5308716d89263e9c7b0e20910f30bc86fe1bf8b
Mateus Rocha <mts_rocha2009@hotmail.com>
2024-09-09 17:39:22 -0300

    Adicionando Serviços

--- lgpd-front/src/app/pages/evaluation/evaluation.service.ts
+++ lgpd-front/src/app/pages/evaluation/evaluation.service.ts
@@ -1,58 +1,58 @@
-import { ErrorHandler, Injectable } from '@angular/core';
-import axios from 'axios';
-import { AxiosInstance } from 'axios';
+import { ErrorHandler, Injectable  } from "@angular/core";
+import axios from "axios";
+import { AxiosInstance } from "axios";
 
-export interface Params {
+export interface Params{
     [key: string]: any;
 }
 
-export interface GetOptions {
+export interface GetOptions{
     url: string;
     params?: Params;
     data?: any;
 }
 
-export interface ErrorResponse {
+export interface ErrorResponse{
     id: string;
     code: string;
     message: string;
 }
 
 @Injectable({
-    providedIn: 'root'
+   providedIn: 'root'
 })
-export class EvaluationService {
+
+
+export class EvaluationService{
+
 
     private axiosClient: AxiosInstance;
     private errorHandler: ErrorHandler;
 
-    constructor(errorHandler: ErrorHandler) {
+    constructor(errorHandler: ErrorHandler){
         this.errorHandler = errorHandler;
         this.axiosClient = axios.create({
             timeout: 3000,
             headers: {
-                "X-Initialized-At": Date.now().toString()
-
+                "X-initializes-at": Date.now.toString()
             }
         });
     }
 
-    // get
-    public async get<T> (options: GetOptions ) : Promise<T> {
+    public async get<T> (options: GetOptions) : Promise<T>{
         try {
             let axiosResponse = await this.axiosClient.request<T>({
                 method: "get",
                 url: options.url,
                 params: options.params
             });
-            return ( axiosResponse.data);
+            return ( axiosResponse.data)
         } catch (error) {
-            return ( Promise.reject( this.normalizeError (error)));
+            return (Promise.reject(this.normalizeError (error)));
         }
     }
 
-    // put
-    public async put<T> (options: GetOptions ) : Promise<T> {
+    public async put<T> (options: GetOptions) : Promise<T>{
         try {
             let axiosResponse = await this.axiosClient.request<T>({
                 method: "put",
@@ -60,14 +60,13 @@ export class EvaluationService {
                 params: options.params,
                 data: options.data
             });
-            return ( axiosResponse.data);
+            return ( axiosResponse.data)
         } catch (error) {
-            return ( Promise.reject( this.normalizeError (error)));
+            return (Promise.reject(this.normalizeError (error)));
         }
     }
 
-    // post
-    public async post<T> (options: GetOptions ) : Promise<T> {
+    public async post<T> (options: GetOptions) : Promise<T>{
         try {
             let axiosResponse = await this.axiosClient.request<T>({
                 method: "post",
@@ -75,36 +74,33 @@ export class EvaluationService {
                 params: options.params,
                 data: options.data
             });
-            return ( axiosResponse.data);
+            return ( axiosResponse.data)
         } catch (error) {
-            return ( Promise.reject( this.normalizeError (error)));
+            return (Promise.reject(this.normalizeError (error)));
         }
     }
 
-    // delete
-    public async delete<T> (options: GetOptions ) : Promise<T> {
+    public async delete<T> (options: GetOptions) : Promise<T>{
         try {
             let axiosResponse = await this.axiosClient.request<T>({
                 method: "delete",
                 url: options.url,
                 params: options.params
             });
-            return ( axiosResponse.data);
+            return ( axiosResponse.data)
         } catch (error) {
-            return ( Promise.reject( this.normalizeError (error)));
+            return (Promise.reject(this.normalizeError (error)));
         }
     }
 
-    // normalização de erros
-    private normalizeError(error: any) : ErrorResponse {
-        console.log('Error: ', error)
+    private normalizeError(error: any): ErrorResponse{
+        console.log('Error ', error);
         this.errorHandler.handleError(error);
-
-        return ({
+        
+        return({
             id: "-1",
-            code: "UnkownError",
-            message: "An unexpected error occurred."
+            code: "Unkown Error",
+            message: "An Unexpected Error Ocurred"
         })
     }
-
 }
--- lgpd-front/src/app/shared/shared.service.ts
+++ lgpd-front/src/app/shared/shared.service.ts
@@ -4,51 +4,65 @@ import { map, Observable, of } from "rxjs";
 import { CourseService } from "../pages/course/course.service";
 import { UserService } from "../pages/user/user.service";
 
-//Criar esses exports
-export interface Params {
-    [key: string]: any;
+
+export interface Params{
+  [key: string]: any;
 }
 
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
+
 export class SharedService {
 
-    users: Array<{ id: String; first_name: String; }> = [];
-    courses: Array<{ id: String; name: String }> = [];
+    users: Array <{value:string, label:string}> = [];
+    courses: Array <{value:string, label:string}> = [];
+    http: any;
 
-    userLabel: Array<{ value: String; label: String; }> = [];
-    courseLabel: Array<{ value: String; label: String; }> = [];
-    
-    //Conversores
+    getUsers(): Observable<any[]>{
 
-    async convertUserToOption(): Promise<any[]> {
-        this.users.forEach((user: { id: String, first_name: String; }) => {
-          let u = {
-            value: user.id.toString(),
-            label: user.first_name
-          }
-          this.userLabel.push(u);
-        });
-        return this.userLabel;
-    }
-    
-    async convertCourseToOption(): Promise<any[]> {
-        this.courses.forEach((course: { id: String, name: String; }) => {
-          let c = {
-            value: course.id.toString(),
-            label: course.name
-          }
-          this.courseLabel.push(c);
-        });
-        return this.courseLabel;
+        return this.http.get("http://localhost:3000/getAllUsers").pipe(
+
+           map(x => {
+               Object.values(x).map((_user) => {
+                  let u = { value: _user.id, label: _user.first_name}
+                  this.users.push(u);
+               })
+
+               console.log(x);
+               console.log(this.users);
+               return this.users;
+           }
+           )
+        )
     }
 
+    getCourses(): Observable<any[]>{
 
-    constructor(
-        private userService: UserService,
-        private courseServoce: CourseService
-    ) {
-    }
+      return this.http.get("http://localhost:3000/getAllCourses").pipe(
+
+         map(x => {
+             Object.values(x).map((_course) => {
+                let u = { value: _course.id, label: _course.first_name}
+                this.courses.push(u);
+             })
+
+             console.log(x);
+             console.log(this.courses);
+             return this.courses;
+         }
+         )
+      )
+  }
+
+  constructor(
+     private http: HttpClient
+  ){
+
+  }
+
+
 }
+
+
